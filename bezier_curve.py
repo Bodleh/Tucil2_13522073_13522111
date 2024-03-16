c441fd51d8448e424f90cbbeae2b385d9a75c1a3
Bodleh <tfprince079@gmail.com>
2024-03-16 16:21:48 +0700

    feat: add bezier bruteforce

--- bezier_curve.py
+++ bezier_curve.py
@@ -52,3 +52,20 @@ def bezier_divide_and_conquer_animate(
     second_half_bezier, all_midpoints = bezier_divide_and_conquer_animate(second_half, iterations - 1, all_midpoints)
     
     return first_half_bezier + second_half_bezier[1:], all_midpoints
+
+def de_casteljau(points: List[Tuple[int, int]], t: float) -> Tuple[int, int]:
+    if len(points) == 1:
+        return points[0]
+    else:
+        new_points = []
+        for i in range(len(points) - 1):
+            new_point = ((1 - t) * points[i][0] + t * points[i + 1][0], (1 - t) * points[i][1] + t * points[i + 1][1])
+            new_points.append(new_point)
+        return de_casteljau(new_points, t)
+
+def bezier_bruteforce(control_points: List[Tuple[int, int]], num_points: int) -> List[Tuple[int, int]]:
+    curve_points = []
+    for i in range(num_points):
+        t = i / (num_points - 1)
+        curve_points.append(de_casteljau(control_points, t))
+    return curve_points
