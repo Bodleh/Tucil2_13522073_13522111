5f42cd6ba015f18892b089dea3e597f557121bf1
Juan A <140235896+juanaw6@users.noreply.github.com>
2024-03-16 17:06:59 +0700

    feat: Add unit test, change main

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ import time
 from bezier_curve import bezier_divide_and_conquer_animate
 from util import valid_int_input
 
-# # User input with color
-# init(autoreset=True)
-# print(Fore.LIGHTYELLOW_EX + "\nBézier Curve Generator\n")
-# n = valid_int_input(Fore.LIGHTGREEN_EX + "Number of control points (>=1): ", has_limit=True, lower_limit=1)
-# control_points = []
-# for i in range(n):
-#     print(Fore.LIGHTWHITE_EX + f"\n== Control Point {i + 1} ==")
-#     x = valid_int_input(Fore.WHITE + f"x{i + 1}: ")
-#     y = valid_int_input(Fore.WHITE + f"y{i + 1}: ")
-#     control_points.append((x, y))
-# iterations = valid_int_input(Fore.LIGHTGREEN_EX + "\nNumber of iterations (>=1): ", has_limit=True, lower_limit=1)
+# User input with color
+init(autoreset=True)
+print(Fore.LIGHTYELLOW_EX + "\nBézier Curve Generator\n")
+n = valid_int_input(Fore.LIGHTGREEN_EX + "Number of control points (>=1): ", has_limit=True, lower_limit=1)
+control_points = []
+for i in range(n):
+    print(Fore.LIGHTWHITE_EX + f"\n== Control Point {i + 1} ==")
+    x = valid_int_input(Fore.WHITE + f"x{i + 1}: ")
+    y = valid_int_input(Fore.WHITE + f"y{i + 1}: ")
+    control_points.append((x, y))
+iterations = valid_int_input(Fore.LIGHTGREEN_EX + "\nNumber of iterations (>=1): ", has_limit=True, lower_limit=1)
 
-# Without user input
-control_points = [(0, 0), (100, 200), (300, 300), (500, 0), (300, -300), (-100, -200), (-200, 100)]
-iterations = 4
+# # Without user input
+# control_points = [(0, 0), (100, 200), (300, 300), (500, 0), (300, -300), (-100, -200), (-200, 100)]
+# iterations = 4
 
 start = time.time()
 curve_points, midpoints = bezier_divide_and_conquer_animate(control_points=control_points, iterations=iterations)
--- test.py
+++ test.py
@@ -1,37 +1,54 @@
-from bezier_curve import bezier_divide_and_conquer, bezier_bruteforce
 import time
 import matplotlib.pyplot as plt
+from bezier_curve import bezier_divide_and_conquer, bezier_bruteforce
 
-test1= [(0, 0), (100, 200), (300, 300), (500, 0), (300, -300), (-100, -200), (-200, 100)]
-test2 = [(0, 0), (100, 200), (300, 0)]
-iterations = 20
+# Test cases
+tests = [
+    ([(0, 0), (100, 200), (300, 300), (500, 0)], 20),
+    ([(0, 0), (100, 200), (200, 0), (300, 200), (400, 0)], 20),
+    ([(0, 0), (100, 100), (200, 200), (300, 300), (400, 400), (500, 500)], 15),
+    ([(0, 0), (50, 150), (100, 0), (150, 150), (200, 0)], 15),
+    ([(0, 0), (200, 400), (400, 0), (600, 400), (800, 0)], 15),
+    ([(0, 0), (100, 300), (200, 100), (300, 300), (400, 100), (500, 300)], 20)
+]
 
-control_points = test1
+# Show graph toggle
+show_graph = False
 
-num_points = 3
-for i in range(iterations - 1) :
-    num_points += num_points - 1
+# Test
+for test_num, (control_points, iterations) in enumerate(tests, start=1):
+    print(f"\nTest {test_num}:")
+    print(f"Number of Control Points: {len(control_points)}")
+    print(f"Control Points: {control_points}")
+    print(f"Midpoint DnC Iterations: {iterations}")
 
-print(num_points)
+    num_points = 2 ** iterations + 1
 
-start = time.time()
-curve_dnc = bezier_divide_and_conquer(control_points=control_points, iterations=iterations)
-end = time.time() - start
-print(f"Divide and Conquer time: {end * 1000}ms")
+    # Divide and Conquer
+    start = time.time()
+    curve_dnc = bezier_divide_and_conquer(control_points=control_points, iterations=iterations)
+    end = time.time() - start
+    print(f"Divide and Conquer time: {end * 1000:.2f}ms")
 
-start = time.time()
-curve_bf = bezier_bruteforce(control_points=control_points, num_points=num_points)
-end = time.time() - start
-print(f"Bruteforce time: {end * 1000}ms")
+    # Bruteforce
+    start = time.time()
+    curve_bf = bezier_bruteforce(control_points=control_points, num_points=num_points)
+    end = time.time() - start
+    print(f"Bruteforce time: {end * 1000:.2f}ms")
 
-# Plot curve
-x, y = zip(*curve_bf)
-plt.plot(x, y, label='Bézier Curve')
+    if show_graph:
+        # Plot curve from Divide and Conquer
+        x_dnc, y_dnc = zip(*curve_dnc)
+        plt.plot(x_dnc, y_dnc, label='Bézier Curve (D&C)', color='blue')
 
-# Plot control points
-control_x, control_y = zip(*control_points)
-plt.scatter(control_x, control_y, color='red', label='Control Points')
-plt.plot(control_x, control_y, color='red', linestyle='dashed', alpha=0.5)
+        # Plot curve from Bruteforce
+        x_bf, y_bf = zip(*curve_bf)
+        plt.plot(x_bf, y_bf, label='Bézier Curve (Bruteforce)', color='green', linestyle='dotted')
 
-plt.legend()
-plt.show()
+        control_x, control_y = zip(*control_points)
+        plt.scatter(control_x, control_y, color='red', label='Control Points')
+        plt.plot(control_x, control_y, color='red', linestyle='dashed', alpha=0.5)
+
+        plt.legend()
+        plt.title(f"Test {test_num}")
+        plt.show()
