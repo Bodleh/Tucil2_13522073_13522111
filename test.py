b8e2898f8ed52b643e1214b551e93a5408b758c2
Juan A <140235896+juanaw6@users.noreply.github.com>
2024-03-17 03:55:51 +0700

    feat: Fix unit test

--- test.py
+++ test.py
@@ -4,16 +4,16 @@ from bezier_curve import bezier_divide_and_conquer, bezier_bruteforce
 
 # Test cases
 tests = [
-    ([(0, 0), (100, 200), (300, 300), (500, 0)], 20),
-    ([(0, 0), (100, 200), (200, 0), (300, 200), (400, 0)], 20),
-    ([(0, 0), (100, 100), (200, 200), (300, 300), (400, 400), (500, 500)], 15),
-    ([(0, 0), (50, 150), (100, 0), (150, 150), (200, 0)], 15),
-    ([(0, 0), (200, 400), (400, 0), (600, 400), (800, 0)], 15),
-    ([(0, 0), (100, 300), (200, 100), (300, 300), (400, 100), (500, 300)], 20)
+    ([(0, 0), (100, 200), (300, 300), (500, 0)], 15),
+    ([(0, 0), (100, 200), (200, 0), (300, 200), (400, 0)], 15),
+    ([(0, 0), (100, 300), (200, -100), (300, 400), (400, 0), (500, 300)], 15),
+    ([(0, 0), (200, 200), (400, 100), (600, 300), (800, 0), (1000, 400)], 15),
+    ([(0, 0), (400, 600), (1000, 400), (1000, -400), (400, -800), (-400, -400)], 15),
+    ([(0, 0), (-300, 0), (0, -300), (300,0), (200, 200), (0, 200), (-100, 100)], 15)
 ]
 
 # Show graph toggle
-show_graph = False
+show_graph = True
 
 # Test
 for test_num, (control_points, iterations) in enumerate(tests, start=1):
@@ -27,27 +27,27 @@ for test_num, (control_points, iterations) in enumerate(tests, start=1):
     # Divide and Conquer
     start = time.time()
     curve_dnc = bezier_divide_and_conquer(control_points=control_points, iterations=iterations)
-    end = time.time() - start
-    print(f"Divide and Conquer time: {end * 1000:.2f}ms")
+    end_dnc = time.time() - start
+    print(f"Divide and Conquer time: {end_dnc * 1000:.2f}ms")
 
     # Bruteforce
     start = time.time()
     curve_bf = bezier_bruteforce(control_points=control_points, num_points=num_points)
-    end = time.time() - start
-    print(f"Bruteforce time: {end * 1000:.2f}ms")
+    end_bf = time.time() - start
+    print(f"Bruteforce time: {end_bf * 1000:.2f}ms")
 
     if show_graph:
         # Plot curve from Divide and Conquer
         x_dnc, y_dnc = zip(*curve_dnc)
-        plt.plot(x_dnc, y_dnc, label='Bézier Curve (D&C)', color='blue')
+        plt.plot(x_dnc, y_dnc, label=f"Bézier Curve (DnC) {end_dnc * 1000:.2f}ms", color='black')
 
         # Plot curve from Bruteforce
         x_bf, y_bf = zip(*curve_bf)
-        plt.plot(x_bf, y_bf, label='Bézier Curve (Bruteforce)', color='green', linestyle='dotted')
+        plt.plot(x_bf, y_bf, label=f"Bézier Curve (Bruteforce) {end_bf * 1000:.2f}ms", color='yellow', linestyle='dashed')
 
         control_x, control_y = zip(*control_points)
         plt.scatter(control_x, control_y, color='red', label='Control Points')
-        plt.plot(control_x, control_y, color='red', linestyle='dashed', alpha=0.5)
+        plt.plot(control_x, control_y, color='red', linestyle='dotted', alpha=0.5)
 
         plt.legend()
         plt.title(f"Test {test_num}")
