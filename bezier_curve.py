1371df7d8908eb704ca38a64547e11a59f4710b0
Bodleh <tfprince079@gmail.com>
2024-03-18 15:53:38 +0700

    fix: optimize dnc

--- bezier_curve.py
+++ bezier_curve.py
@@ -7,23 +7,20 @@ def bezier_divide_and_conquer(
     control_points: List[Tuple[int, int]], 
     iterations: int
 ) -> List[Tuple[int, int]]:
-    if iterations == 0:
+    if (iterations == 0):
         return [control_points[0], control_points[-1]]
-    
-    first_half = [control_points[0]]
-    second_half = [control_points[-1]]
-    n = len(control_points)
-    while n > 1:
-        midpoints = [midpoint(control_points[i], control_points[i + 1]) for i in range(len(control_points) - 1)]
-        control_points = midpoints
-        first_half.append(midpoints[0])
-        second_half = [midpoints[-1]] + second_half
-        n -= 1
-    
-    first_half_bezier = bezier_divide_and_conquer(first_half, iterations - 1)
-    second_half_bezier = bezier_divide_and_conquer(second_half, iterations - 1)
-    
-    return first_half_bezier + second_half_bezier[1:]
+    else:
+        first_half = [control_points[0]]
+        second_half = [control_points[-1]]
+        for _ in range(len(control_points), 1, -1):
+            midpoints = [midpoint(control_points[i], control_points[i + 1]) for i in range(len(control_points) - 1)]
+            control_points = midpoints
+            first_half.append(midpoints[0])
+            second_half.insert(0, midpoints[-1])
+        
+        first_half_bezier = bezier_divide_and_conquer(first_half, iterations - 1)
+        second_half_bezier = bezier_divide_and_conquer(second_half, iterations - 1)
+        return first_half_bezier + second_half_bezier[1:]
 
 def bezier_divide_and_conquer_animate(
     control_points: List[Tuple[int, int]], 
@@ -35,25 +32,23 @@ def bezier_divide_and_conquer_animate(
 
     if iterations == 0:
         return [control_points[0], control_points[-1]], all_midpoints
-    
-    all_mid = [control_points]
-    first_half = [control_points[0]]
-    second_half = [control_points[-1]]
-    n = len(control_points)
-    while n > 1:
-        midpoints = [midpoint(control_points[i], control_points[i + 1]) for i in range(len(control_points) - 1)]
-        all_mid.append(midpoints)
-        control_points = midpoints
-        first_half.append(midpoints[0])
-        second_half = [midpoints[-1]] + second_half
-        n -= 1
-    
-    all_midpoints.append(all_mid)
+    else:
+        all_mid = [control_points]
+        first_half = [control_points[0]]
+        second_half = [control_points[-1]]
+        for _ in range(len(control_points), 1, -1):
+            midpoints = [midpoint(control_points[i], control_points[i + 1]) for i in range(len(control_points) - 1)]
+            all_mid.append(midpoints)
+            control_points = midpoints
+            first_half.append(midpoints[0])
+            second_half.insert(0, midpoints[-1])
+        
+        all_midpoints.append(all_mid)
 
-    first_half_bezier, all_midpoints = bezier_divide_and_conquer_animate(first_half, iterations - 1, all_midpoints)
-    second_half_bezier, all_midpoints = bezier_divide_and_conquer_animate(second_half, iterations - 1, all_midpoints)
-    
-    return first_half_bezier + second_half_bezier[1:], all_midpoints
+        first_half_bezier, all_midpoints = bezier_divide_and_conquer_animate(first_half, iterations - 1, all_midpoints)
+        second_half_bezier, all_midpoints = bezier_divide_and_conquer_animate(second_half, iterations - 1, all_midpoints)
+        
+        return first_half_bezier + second_half_bezier[1:], all_midpoints
 
 def de_casteljau(points: List[Tuple[int, int]], t: float) -> Tuple[int, int]:
     if len(points) == 1:
